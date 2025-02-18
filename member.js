d000fcc875c324e421fef297f6a9b7dc979382f5
nvilar3 <nestorvl2011@hotmail.com>
2025-02-18 15:50:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    return (
+        <div className="skillsMember">
+            <h1>Skills</h1>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+            </ul>
+        </div>
+    );
+}
